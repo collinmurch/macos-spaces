6072c5f2133c01bd86215f7dc215cd295562cd75
Collin Murch <collin@murch.net>
2019-01-20 12:08:26 -0800

    Initial Commit

---
+++ macos-spaces/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macos-spaces
+//
+//  Created by Collin Murch on 1/20/19.
+//  Copyright © 2019 collinmurch. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macos-spaces/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macos-spaces
+//
+//  Created by Collin Murch on 1/20/19.
+//  Copyright © 2019 collinmurch. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

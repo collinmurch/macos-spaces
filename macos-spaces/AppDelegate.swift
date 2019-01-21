63aa5d53c261e4df51b02856d8017d29f6321a9c
Collin Murch <collin@murch.net>
2019-01-20 17:31:38 -0800

    Init commit

--- macos-spaces/AppDelegate.swift
+++ macos-spaces/AppDelegate.swift
@@ -11,8 +11,22 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    var statusBar = NSStatusBar.system.statusItem(withLength: -1)
+    var menu: NSMenu = NSMenu()
+    var menuItem : NSMenuItem = NSMenuItem()
 
-
+    override func awakeFromNib() {
+        statusBar.menu = menu
+        statusBar.button?.title = "Presses"
+        
+        menuItem.title = "Clicked"
+        menu.addItem(menuItem)
+    }
+    
+    func update(_ text: String) {
+        statusBar.button?.title = "\(text)"
+    }
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
     }
--- macos-spaces/ViewController.swift
+++ macos-spaces/ViewController.swift
@@ -10,12 +10,25 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var button: NSButton!
+    @IBOutlet weak var label: NSTextField!
+    @IBOutlet var window: NSView!
+    
+    let ad = NSApplication.shared.delegate as? AppDelegate
+    
+    public var count: Int64 = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func clicked(_ sender: Any) {
+        count += 1
+        
+        label.stringValue = String(count)
+        ad?.update(String(count))
+    }
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.

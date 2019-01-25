c150b2ca289b1a0d63f6d8c6a6ff2f8bd7571343
Collin Murch <collin@murch.net>
2019-01-25 12:00:02 -0800

    Added number support

--- macos-spaces/AppDelegate.swift
+++ macos-spaces/AppDelegate.swift
@@ -23,7 +23,7 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    var statusBar: NSStatusItem = NSStatusBar.system.statusItem(withLength: -1)
+    var statusBar: NSStatusItem = NSStatusBar.system.statusItem(withLength: 100)
     var menu: NSMenu = NSMenu()
     var menuItem: NSMenuItem = NSMenuItem()
     
@@ -31,30 +31,53 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         statusBar.menu = menu
         statusBar.button?.title = "Presses"
         
+        statusBar.button?.image = generateImage()
+        
+        menuItem.title = "Collin's app"
+        menu.addItem(menuItem)
+    }
+    
+    func generateImage() -> NSImage {
         let img: NSImage = NSImage(size: NSSize.init(width: 68.0, height: 15.0))
         
-        let red = NSColor.white
+        let bColor = NSColor.white
+        let tColor = NSColor.black
         
-        var rect: NSRect!
+        var text: String
+        let font = NSFont(name: "Helvetica Bold", size: 12.0)
+        let textStyle = NSMutableParagraphStyle.default.mutableCopy() as! NSMutableParagraphStyle
+        textStyle.alignment = NSTextAlignment.center
+        
+        
+        let attributeDict: [NSAttributedString.Key : Any] = [
+            .font: font!,
+            .foregroundColor: tColor,
+            .paragraphStyle: textStyle,
+            ]
         
         for i in 1...4 {
-            rect = NSMakeRect(0.0 + CGFloat(i-1)*17.0, 0, 15, 15)
+            text = "\(i)"
+            
+            let rect = NSMakeRect(0.0 + CGFloat(i-1)*17.0, 0, 15, 15)
+            let textRect = NSMakeRect(-5 + CGFloat(i-1)*17.0, -11, 25, 25)
+            
+            let path: NSBezierPath = NSBezierPath(roundedRect: rect, xRadius: 2.5, yRadius: 2.5)
             
             img.lockFocus()
             
-            let path: NSBezierPath = NSBezierPath(roundedRect: rect, xRadius: 5, yRadius: 5)
-            
-            red.set()
-            
+            bColor.set()
             path.fill()
+            
+            img.unlockFocus()
+            
+            img.lockFocus()
+            
+            text.draw(in: textRect, withAttributes: attributeDict)
             
             img.unlockFocus()
         }
         
-        statusBar.button?.image = img
-        
-        menuItem.title = "Collin's app"
-        menu.addItem(menuItem)
+        return img
     }
     
     func update(_ text: String) {
